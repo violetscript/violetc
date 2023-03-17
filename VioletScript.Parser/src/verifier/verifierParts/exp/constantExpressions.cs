5ac7633fbb9d7dee8620dbcc7068eae9b71d1cf4
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-03-17 11:43:18 -0300

    _

--- VioletScript.Parser/src/verifier/verifierParts/exp/constantExpressions.cs
+++ VioletScript.Parser/src/verifier/verifierParts/exp/constantExpressions.cs
@@ -115,6 +115,86 @@ public partial class Verifier
                 return r;
             }
         } // Identifier
+        // verify member; ensure
+        // - it is not undefined.
+        // - it is lexically visible.
+        // - if it is a non-argumented generic type or function, throw a VerifyError.
+        // - it is a compile-time value.
+        if (exp is Ast.MemberExpression memb && !memb.Optional)
+        {
+            var @base = VerifyConstantExp(memb.Base, faillible, null, false);
+            if (@base == null)
+            {
+                exp.SemanticSymbol = null;
+                exp.SemanticConstantExpResolved = true;
+                return exp.SemanticSymbol;
+            }
+            var r = @base.ResolveProperty(memb.Id.Name);
+            if (r == null)
+            {
+                // VerifyError: undefined reference
+                if (faillible)
+                {
+                    VerifyError(null, 128, memb.Id.Span.Value, new DiagnosticArguments { ["name"] = memb.Id.Name });
+                }
+                exp.SemanticSymbol = null;
+                exp.SemanticConstantExpResolved = true;
+                return exp.SemanticSymbol;
+            }
+            else
+            {
+                if (!r.PropertyIsVisibleTo(m_Frame))
+                {
+                    // VerifyError: accessing private property
+                    if (faillible)
+                    {
+                        VerifyError(null, 130, memb.Id.Span.Value, new DiagnosticArguments { ["name"] = memb.Id.Name });
+                    }
+                    exp.SemanticSymbol = null;
+                    exp.SemanticConstantExpResolved = true;
+                    return exp.SemanticSymbol;
+                }
+                r = r is Alias ? r.AliasToSymbol : r;
+                // VerifyError: unargumented generic type or function
+                if (!instantiatingGeneric && r.TypeParameters != null)
+                {
+                    if (faillible)
+                    {
+                        VerifyError(null, 132, memb.Id.Span.Value, new DiagnosticArguments { ["name"] = memb.Id.Name });
+                    }
+                    exp.SemanticSymbol = null;
+                    exp.SemanticConstantExpResolved = true;
+                    return exp.SemanticSymbol;
+                }
+
+                // extend variable life
+                if (r is ReferenceValueFromFrame && r.Base.FindActivation() != m_Frame.FindActivation())
+                {
+                    r.Base.FindActivation().AddExtendedLifeVariable(r.Property);
+                }
+
+                // use initial constant value if any.
+                if ((r is ReferenceValue || r is ReferenceValueFromNamespace || r is ReferenceValueFromType) && r.Property.InitValue is ConstantValue)
+                {
+                    r = r.Property.InitValue;
+                }
+
+                if  (!(r is Type || r is ConstantValue || r is Namespace))
+                {
+                    if (faillible)
+                    {
+                        VerifyError(null, 151, memb.Id.Span.Value, new DiagnosticArguments { ["name"] = memb.Id.Name });
+                    }
+                    exp.SemanticSymbol = null;
+                    exp.SemanticConstantExpResolved = true;
+                    return exp.SemanticSymbol;
+                }
+
+                exp.SemanticSymbol = r;
+                exp.SemanticConstantExpResolved = true;
+                return r;
+            }
+        } // MemberExpression
         else
         {
             if (faillible)
