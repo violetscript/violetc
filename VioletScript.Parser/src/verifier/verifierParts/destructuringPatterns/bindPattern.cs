02cc93c665a5b8d450acc41d069c3e6002f81db3
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-06-23 14:16:20 -0300

    _

--- VioletScript.Parser/src/verifier/verifierParts/destructuringPatterns/bindPattern.cs
+++ VioletScript.Parser/src/verifier/verifierParts/destructuringPatterns/bindPattern.cs
@@ -53,6 +53,11 @@ public partial class Verifier
         {
             // VerifyError: duplicate definition
             newDefinition = previousDefinition is VariableSlot ? previousDefinition : null;
+            // assert newDefinition != null
+            if (newDefinition == null)
+            {
+                throw new Exception("Duplicating definition with wrong kind.");
+            }
             if (!m_Options.AllowDuplicates)
             {
                 VerifyError(pattern.Span.Value.Script, 139, pattern.Span.Value, new DiagnosticArguments { ["name"] = pattern.Name });
