71e5af189aaa2ba145afc32f93187733edb8dd4b
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-06-27 15:16:00 -0300

    _

--- parser/src/verifier/parts/exp/optionalChaining.cs
+++ parser/src/verifier/parts/exp/optionalChaining.cs
@@ -46,122 +46,37 @@ public partial class Verifier
         }
 
         var nonNullBase = m_ModelCore.Factory.Value(baseType.ToNonNullableType());
-        memb.SemanticNonNullBase = nonNullBase;
+        optChaining.SemanticNonNullBase = nonNullBase;
 
-        var r = nonNullBase.ResolveProperty(memb.Id.Name);
-        memb.SemanticOptNonNullUnifiedSymbol = r;
-
-        var chainingPlaceholder = memb.OptChain.FindOptionalChainingPlaceholder();
-        chainingPlaceholder.SemanticSymbol = memb.SemanticNonNullBase;
+        var chainingPlaceholder = optChaining.OptChain.FindOptionalChainingPlaceholder();
+        chainingPlaceholder.SemanticSymbol = optChaining.SemanticNonNullBase;
         chainingPlaceholder.SemanticExpResolved = true;
-        toDo();
-    }
 
-    // verifies member; ensure
-    // - it is not undefined.
-    // - it is lexically visible.
-    // - if it is a non-argumented generic type or function, throw a VerifyError.
-    // - it is a compile-time value.
-    private Symbol VerifyOptMemberExp
-    (
-        Ast.OptMemberExpression memb,
-        Symbol expectedType,
-        bool instantiatingGeneric
-    )
-    {
-        Ast.Expression exp = memb;
-        var @base = VerifyExp(memb.Base, null, false);
-        if (@base == null)
-        {
-            exp.SemanticSymbol = null;
-            exp.SemanticExpResolved = true;
-            return exp.SemanticSymbol;
-        }
-        if (!(@base is Value))
-        {
-            // VerifyError: optional member must have a value base
-            VerifyError(null, 169, memb.Base.Span.Value, new DiagnosticArguments {});
-            exp.SemanticSymbol = null;
-            exp.SemanticExpResolved = true;
-            return exp.SemanticSymbol;
-        }
-        var baseType = @base.StaticType;
-        if (!baseType.IncludesNull && !baseType.IncludesUndefined)
-        {
-            // VerifyError: optional member base must possibly be undefined
-            // or null.
-            VerifyError(null, 170, memb.Base.Span.Value, new DiagnosticArguments {["t"] = baseType});
-            exp.SemanticSymbol = null;
-            exp.SemanticExpResolved = true;
-            return exp.SemanticSymbol;
-        }
-
-        var nonNullBase = m_ModelCore.Factory.Value(baseType.ToNonNullableType());
-        memb.SemanticNonNullBase = nonNullBase;
-
-        var r = nonNullBase.ResolveProperty(memb.Id.Name);
-        memb.SemanticOptNonNullUnifiedSymbol = r;
-
-        var chainingPlaceholder = memb.OptChain.FindOptionalChainingPlaceholder();
-        chainingPlaceholder.SemanticSymbol = memb.SemanticNonNullBase;
-        chainingPlaceholder.SemanticExpResolved = true;
-        toDo();
+        var r = VerifyExpAsValue(optChaining.OptChain);
+        optChaining.SemanticOptNonNullUnifiedResult = r;
 
         if (r == null)
         {
-            // VerifyError: undefined reference
-            ReportNameNotFound(memb.Id.Name, memb.Id.Span.Value, nonNullBase);
             exp.SemanticSymbol = null;
             exp.SemanticExpResolved = true;
             return exp.SemanticSymbol;
         }
+
+        // unify to null and/or undefined
+        if (baseType.IncludesNull && !baseType.IncludesUndefined)
+        {
+            exp.SemanticSymbol = m_ModelCore.Factory.Value(m_ModelCore.Factory.UnionType(new Symbol[]{m_ModelCore.NullType, r.StaticType}));
+        }
+        else if (!baseType.IncludesNull && baseType.IncludesUndefined)
+        {
+            exp.SemanticSymbol = m_ModelCore.Factory.Value(m_ModelCore.Factory.UnionType(new Symbol[]{m_ModelCore.UndefinedType, r.StaticType}));
+        }
         else
         {
-            if (!r.PropertyIsVisibleTo(m_Frame))
-            {
-                // VerifyError: accessing private property
-                VerifyError(null, 130, memb.Id.Span.Value, new DiagnosticArguments { ["name"] = memb.Id.Name });
-                exp.SemanticSymbol = null;
-                exp.SemanticExpResolved = true;
-                return exp.SemanticSymbol;
-            }
-            r = r?.EscapeAlias();
-            // VerifyError: unargumented generic type or function
-            if (!instantiatingGeneric && r.IsGenericTypeOrMethod)
-            {
-                VerifyError(null, 132, memb.Id.Span.Value, new DiagnosticArguments { ["name"] = memb.Id.Name });
-                exp.SemanticSymbol = null;
-                exp.SemanticExpResolved = true;
-                return exp.SemanticSymbol;
-            }
-
-            // extend variable life
-            if (r is ReferenceValueFromFrame && r.Base.FindActivation() != m_Frame.FindActivation())
-            {
-                r.Base.FindActivation()?.AddExtendedLifeVariable(r.Property);
-            }
-
-            exp.SemanticExpResolved = true;
-
-            if (r is Value && r.StaticType == null)
-            {
-                VerifyError(null, 199, memb.Id.Span.Value, new DiagnosticArguments {});
-                exp.SemanticSymbol = null;
-            }
-            else if (baseType.IncludesNull && !baseType.IncludesUndefined)
-            {
-                exp.SemanticSymbol = m_ModelCore.Factory.Value(m_ModelCore.Factory.UnionType(new Symbol[]{m_ModelCore.NullType, r.StaticType}));
-            }
-            else if (!baseType.IncludesNull && baseType.IncludesUndefined)
-            {
-                exp.SemanticSymbol = m_ModelCore.Factory.Value(m_ModelCore.Factory.UnionType(new Symbol[]{m_ModelCore.UndefinedType, r.StaticType}));
-            }
-            else
-            {
-                exp.SemanticSymbol = m_ModelCore.Factory.Value(m_ModelCore.Factory.UnionType(new Symbol[]{m_ModelCore.UndefinedType, m_ModelCore.NullType, r.StaticType}));
-            }
-
-            return exp.SemanticSymbol;
+            exp.SemanticSymbol = m_ModelCore.Factory.Value(m_ModelCore.Factory.UnionType(new Symbol[]{m_ModelCore.UndefinedType, m_ModelCore.NullType, r.StaticType}));
         }
-    } // member expression (?.)
+
+        exp.SemanticExpResolved = true;
+        return exp.SemanticSymbol;
+    }
 }
