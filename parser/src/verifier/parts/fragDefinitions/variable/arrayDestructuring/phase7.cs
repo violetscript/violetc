d0eea72f8cc508a607ade857f6f034a81dfe7e97
matheusdiasdesouzads <matheusdiasdesouzads@gmail.com>
2023-06-28 12:06:34 -0300

    _

--- parser/src/verifier/parts/fragDefinitions/variable/arrayDestructuring/phase7.cs
+++ parser/src/verifier/parts/fragDefinitions/variable/arrayDestructuring/phase7.cs
@@ -29,11 +29,19 @@ public partial class Verifier
         }
         else
         {
-            if (type != m_ModelCore.AnyType)
+            var proxy = type.Delegate.Proxies.ContainsKey(Operator.ProxyToGetIndex) ? type.Delegate.Proxies[Operator.ProxyToGetIndex] : null;
+            if (proxy != null && this.m_ModelCore.IsNumericType(proxy.StaticType.FunctionRequiredParameters[0].Type))
             {
-                this.VerifyError(pattern.Span.Value.Script, 141, pattern.Span.Value, new DiagnosticArguments { ["t"] = type });
+                this.Fragmented_VerifyArrayDestructuringPattern7ForProxy(pattern, proxy);
             }
-            this.Fragmented_VerifyArrayDestructuringPattern7ForAny(pattern);
+            else
+            {
+                if (type != m_ModelCore.AnyType)
+                {
+                    this.VerifyError(pattern.Span.Value.Script, 141, pattern.Span.Value, new DiagnosticArguments { ["t"] = type });
+                }
+                this.Fragmented_VerifyArrayDestructuringPattern7ForAny(pattern);
+            }
         }
     }
 
@@ -80,6 +88,33 @@ public partial class Verifier
                     this.VerifyError(spread.Span.Value.Script, 144, spread.Span.Value, new DiagnosticArguments {});
                 }
                 this.Fragmented_VerifyDestructuringPattern7(spread.Pattern, arrayType);
+            }
+            else
+            {
+                this.Fragmented_VerifyDestructuringPattern7((Ast.DestructuringPattern) item, arrayElementType);
+            }
+        }
+    }
+
+    private void Fragmented_VerifyArrayDestructuringPattern7ForProxy(Ast.ArrayDestructuringPattern pattern, Symbol proxy)
+    {
+        var arrayElementType = proxy.StaticType.FunctionReturnType;
+        for (int i = 0; i < pattern.Items.Count(); ++i)
+        {
+            var item = pattern.Items[i];
+            if (item == null)
+            {
+                // ellision
+            }
+            else if (item is Ast.ArrayDestructuringSpread spread)
+            {
+                // a rest element must be the last element
+                if (i != pattern.Items.Count() -1)
+                {
+                    this.VerifyError(spread.Span.Value.Script, 144, spread.Span.Value, new DiagnosticArguments {});
+                }
+                // for a proxy destructuring, the rest will be a [T] type.
+                this.Fragmented_VerifyDestructuringPattern7(spread.Pattern, this.m_ModelCore.Factory.TypeWithArguments(this.m_ModelCore.ArrayType, new Symbol[]{arrayElementType}));
             }
             else
             {
